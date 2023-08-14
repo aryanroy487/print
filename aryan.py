dd456a36bf6ca886aea6a70c8382d574d5866e30
aryanroy487 <141859578+aryanroy487@users.noreply.github.com>
2023-08-15 00:14:41 +0530

    Add files via upload

---
+++ aryan.py
@@ -0,0 +1,8 @@
+Python 3.10.0a5 (tags/v3.10.0a5:b0478d7, Feb  3 2021, 01:24:21) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 	print("hello world")
+	
+SyntaxError: unexpected indent
+>>> print("hello world")
+hello world
+>>> 
